9e641814e8576561baace670e15048677fc23915
Hessel Vroege <hesselvroege@hotmail.com>
2012-03-01 16:24:01 +0100

    Remake shader file with proper funcs and flexibility

--- gpuraytrace/shader/csmain.hlsl
+++ gpuraytrace/shader/csmain.hlsl
@@ -1,12 +1,9 @@
 #include "noise.hlsl"
-//SamplerState state;
-//Texture2D<float> texNoise;
 
 /*cbuffer CBTweakable
 {
 	float3 SunDirection;
 }*/
-
 
 cbuffer CBFrame
 {
@@ -20,128 +17,146 @@ cbuffer CBPermanent
 	float2 ScreenSize;
 }
 
-const static float RAY_STEP = 0.1f;
-const static int RAY_STEPS = 50000;
-const static float3 SunDirection = float3(0.4f, 0.4f, 0.4f);
+const static float3 FOG_COLOR = float3(0.7f, 0.7f, 0.7f);
+float4 getFog(float3 p)
+{
+	//--Calculate fog here
+	return 0.0f;
+}
 
-RWTexture2D<float4> texOut : register(u0);
-
-float getDensity(float3 position)
+float getDensity(float3 p)
 {
 	float d = 0.0f;
-
-	const static float p = 0.45f;
-	float3 scaled = position * 0.1f;
-	
-	//d = noise3d(scaled) - position.y * 0.1f;
+	float3 scaled = p * 0.1f;
 	
 	for(uint x = 0; x < 2; x++)
 	{
-		d += noise3d(scaled * pow(2, x)) * pow(p, x);
+		d += noise3d(scaled * pow(2, x)) * pow(0.45f, x);
 	}
 	
-	//d += sin(position.y) * 0.4f;
-	d -= position.y * 0.15f;
-	
-	return d;
+	d -= p.y * 0.15f;
 
+	return d;
 }
 
-float4 getSky(float3 direction)
+const static float RAY_STEP = 0.1f;
+const static uint RAY_STEPS = 50000;
+const static float MAX_DIST = RAY_STEP * RAY_STEPS;
+float2 traceRay(float3 p, float stepmod, float3 dir, out float3 fogcol, out float3 pout)
 {
-	float4 c = float4(0.0f, 0.0f, 1.0f - direction.y * 0.6f, 0.0f);
+	float4 f = 0.0;
+	
+	float step = RAY_STEP * stepmod;
+	float s = 0.5f;
+	float d = 0.0f;
+	float3 rayp;
+	[loop] while(s < MAX_DIST && step > RAY_STEP * 0.1f)
+	{
+		rayp = p + dir * s;
+		
+		d = getDensity(rayp);
+		f += getFog(rayp);
+
+		if(f.a > 1.0f || d > 0.0f) 
+		{
+			s -= step;
+			step *= 0.4f;
+		} else {
+			step *= 1.015f;
+			s += step;
+		}
+	}
+	
+	pout = rayp;
+	fogcol = f.rgb;
+	return float2(d, f.a);
+}
+
+const static float3 SunDirection = float3(0.4f, 0.4f, 0.4f);
+const static float3 ShadowColor = float3(0.1f, 0.1f, 0.2f);
+float3 getColor(float3 p, float3 n)
+{
+	float3 color = 0.0f;
+	
+	//Calculate landscape color
+	float h = p.y;
+	h += 100;
+	color = float3(h * 0.01f, h * 0.010f, (h - 80.0f) * 0.03);
+	float brightness = saturate(dot(n, normalize(SunDirection)));
+	color *= brightness;
+	
+	//Calculate shadow
+	float3 fog;
+	float3 pout;
+	float2 d = traceRay(p, 4.0f, SunDirection, fog, pout);
+	
+	if(d.x > 0.0f)
+	{
+		color *= lerp(ShadowColor, fog, d.y);
+	} else {
+		color = lerp(color, fog, d.y);
+	}
+	
+	return color;
+}
+
+float3 getSky(float3 dir)
+{
+	float3 c = float3(0.0f, 0.0f, 1.0f - dir.y * 0.6f);
 	c.rg += (c.b - 0.6f) * 1.0f;
 	
-	float hemi = saturate(dot(direction, normalize(SunDirection)));
+	float hemi = saturate(dot(dir, normalize(SunDirection)));
 	c.rgb += pow(hemi, 14.0f);
 	return c;
 }
 
-//const static float normalDistance = 0.01f;
-float4 getPoint(float3 position)
+float3 getNormal(float3 p, float d)
 {
-	float d = getDensity(position);
-	
-	if(d < 0.0f) return float4(0.0f, 0.0f, 0.0f, 0.0f);
-	
-	float2 normalDistance = float2(length(position - Eye.xyz) * 0.005f, 0.0f);
-	float3 normal = normalize(float3(
-		 getDensity(position - normalDistance.xyy) - d,
-		 getDensity(position - normalDistance.yxy) - d,
-		 getDensity(position - normalDistance.yyx) - d
+	float2 normalDistance = float2(length(p - Eye.xyz) * 0.005f, 0.0f);
+	return normalize(float3(
+		 getDensity(p - normalDistance.xyy) - d,
+		 getDensity(p - normalDistance.yxy) - d,
+		 getDensity(p - normalDistance.yyx) - d
 	));
-	
-	//return float4(normal * 0.5f + 0.5f, 1.0f);
-	
-	float h = position.y;
-	h += 100;
-	float3 color = float3(h * 0.01f, h * 0.010f, (h - 80.0f) * 0.03);
-	//float3 color = float3(fmod(h, 1.0f), 0.0f, 0.0f);
-	float brightness = saturate(dot(normal, normalize(SunDirection)));
-	return float4(color * brightness, 1.0f);
-	//if(h < position.y) return float4(0.0f, 0.0f, 0.0f, 0.0f);
-	//return float4(h * 0.05f, h * 0.1f, saturate(-h) + h * 0.03f, 1.0f);
 }
 
+struct PixelData
+{
+	float3 p;
+	float3 dir;
+};
+
+PixelData getPixelRay(uint2 DTid)
+{
+	float4 screenLocation = float4((DTid / ScreenSize.xy - 0.5f) * 2.0f, 1.0f, 1.0f);
+	screenLocation.x /= Projection._m11;
+	screenLocation.y /= Projection._m22;
+
+	PixelData pd;
+	pd.p = mul(screenLocation, ViewInverse).xyz;
+	pd.dir = normalize(pd.p - Eye.xyz);
+	return pd;
+}
+
+RWTexture2D<float4> texOut : register(u0);
 [numthreads(20, 20, 1)]
 void CSMain( uint3 DTid : SV_DispatchThreadID )
 {
-	float4 result = float4(0.0f, 0.0f, 0.0f, 0.0f);
-
-	float4 screenLocation = float4((DTid.xy / ScreenSize.xy - 0.5f) * 2.0f, 1.0f, 1.0f);
-	screenLocation.x /= Projection._m11;
-	screenLocation.y /= Projection._m22;
+	PixelData pd = getPixelRay(DTid.xy);
 	
-	float3 rayStart = mul(screenLocation, ViewInverse).xyz;
-	float3 rayDirection = normalize(rayStart - Eye.xyz);
-
-	float maxDist = RAY_STEP * RAY_STEPS;
+	float3 p;
+	float3 color;
+	float2 d = traceRay(pd.p, 1.0f, pd.dir, color, p);
 	
-	float step = RAY_STEP;
-	float d = 0.0f;
-	float3 rayPosition;
-	
-	//[loop] while(d < maxDist && step > (d * 0.001f))
-	[loop] while(d < maxDist && step > RAY_STEP * 0.1f)
+	if(d.x > 0.0f) 							//-- we've hit something
 	{
-		rayPosition = rayStart + rayDirection * d;
-		float4 color = getPoint(rayPosition);
-
-		if(color.a >= 0.1)
-		{
-			result = color;
-		
-			d -= step;
-			step *= 0.4f;
-		} else {
-			step *= 1.015f;
-			d += step;
-		}
+		float3 n = getNormal(p, d.x);
+		float3 tcolor = getColor(p, n);
+		color = lerp(tcolor, color, d.y);
+	} else { 								// -- we've hit nothing
+		float3 scolor = getSky(pd.dir);
+		color = lerp(scolor, color, d.y);
 	}
 	
-	if(result.a < 0.1)
-	{
-		result = getSky(rayDirection);
-	}else{
-		/*step = RAY_STEP * 5.0f;
-		d = 0.5f;
-		rayStart = rayPosition;
-		
-		[loop] while(d < maxDist * 0.05f && result.a >= 0.1f)
-		{
-			rayPosition = rayStart + SunDirection * d;
-			float4 color = getPoint(rayPosition);
-
-			if(color.a >= 0.1)
-			{
-				result *= float4(0.1f,0.1f,0.2f,0.0f);
-				//break;
-			} else {
-				step *= 1.05f;
-				d += step;
-			}
-		}*/
-	}
-	
-	texOut[DTid.xy] = result;
+	texOut[DTid.xy] = float4(color, 0.0f);
 }
