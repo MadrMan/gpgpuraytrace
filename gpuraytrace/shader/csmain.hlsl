1bdac2f0d7f3af3d43be4474c8223e8793d67cd0
Patrick Wouterse <madman_49@hotmail.com>
2012-03-01 12:57:32 +0100

    not so great 3d landscape

--- gpuraytrace/shader/csmain.hlsl
+++ gpuraytrace/shader/csmain.hlsl
@@ -26,22 +26,25 @@ const static float3 SunDirection = float3(0.4f, 0.4f, 0.4f);
 
 RWTexture2D<float4> texOut : register(u0);
 
-float getHeight(float2 position)
+float getDensity(float3 position)
 {
+	float d = 0.0f;
+
 	const static float p = 0.45f;
+	float3 scaled = position * 0.1f;
 	
-	float h = 0.3f;
-	float2 scaled = position * 0.01f;
+	//d = noise3d(scaled) - position.y * 0.1f;
 	
-	//return sin(position.x * 0.1f) * sin(position.y * 0.1f) * 20.0f;
 	for(uint x = 0; x < 2; x++)
 	{
-		h += noise3d(float3(scaled * pow(2, x), 0.0f)) * pow(p, x);
+		d += noise3d(scaled * pow(2, x)) * pow(p, x);
 	}
 	
-	//float h = sin(position.x * 0.1f) * 0.1f * sin(position.y * 0.1f);
+	//d += sin(position.y) * 0.4f;
+	d -= position.y * 0.15f;
 	
-	return h * 80.0f;
+	return d;
+
 }
 
 float4 getSky(float3 direction)
@@ -57,24 +60,21 @@ float4 getSky(float3 direction)
 //const static float normalDistance = 0.01f;
 float4 getPoint(float3 position)
 {
-	float h = getHeight(position.xz);
+	float d = getDensity(position);
 	
-	if(h < position.y) return float4(0.0f, 0.0f, 0.0f, 0.0f);
+	if(d < 0.0f) return float4(0.0f, 0.0f, 0.0f, 0.0f);
 	
-	float normalDistance = length(position - Eye.xyz) * 0.005f;
-	
-	position.y = h;
-	float3 herex = float3(position.x - normalDistance, h, position.z);
-	float3 herey = float3(position.x, h, position.z - normalDistance);
-	herex.y = getHeight(herex.xz);
-	herey.y = getHeight(herey.xz);
-	
-	position = position.xzy; herex = herex.xzy; herey = herey.xzy;
-	float3 normal = normalize(cross(position - herex, position - herey));
+	float2 normalDistance = float2(length(position - Eye.xyz) * 0.005f, 0.0f);
+	float3 normal = normalize(float3(
+		 getDensity(position - normalDistance.xyy) - d,
+		 getDensity(position - normalDistance.yxy) - d,
+		 getDensity(position - normalDistance.yyx) - d
+	));
 	
 	//return float4(normal * 0.5f + 0.5f, 1.0f);
 	
-	h += 45;
+	float h = position.y;
+	h += 100;
 	float3 color = float3(h * 0.01f, h * 0.010f, (h - 80.0f) * 0.03);
 	//float3 color = float3(fmod(h, 1.0f), 0.0f, 0.0f);
 	float brightness = saturate(dot(normal, normalize(SunDirection)));
@@ -123,26 +123,24 @@ void CSMain( uint3 DTid : SV_DispatchThreadID )
 	{
 		result = getSky(rayDirection);
 	}else{
-		step = RAY_STEP * 5.0f;
+		/*step = RAY_STEP * 5.0f;
 		d = 0.5f;
 		rayStart = rayPosition;
 		
-		[loop] while(d < maxDist * 0.1f && result.a >= 0.1f)
+		[loop] while(d < maxDist * 0.05f && result.a >= 0.1f)
 		{
 			rayPosition = rayStart + SunDirection * d;
 			float4 color = getPoint(rayPosition);
 
 			if(color.a >= 0.1)
 			{
-				result *= float4(0.2f,0.2f,0.6f,0.0f);
+				result *= float4(0.1f,0.1f,0.2f,0.0f);
 				//break;
 			} else {
 				step *= 1.05f;
 				d += step;
 			}
-		}
-		
-		
+		}*/
 	}
 	
 	texOut[DTid.xy] = result;
