55ac0e8b37c5e167559055e829175958479d565e
Hessel Vroege <hesselvroege@hotmail.com>
2012-05-24 09:04:12 +0200

    set nomadplains as default, some other minor changes

--- gpuraytrace/Media/testing/shaders/terrain.hlsl
+++ gpuraytrace/Media/testing/shaders/terrain.hlsl
@@ -1,6 +1,8 @@
 #define FBM_THIS(OUT, OCT, FUNC) \
 	for(N = 1.0f; N <= OCT; N++) { float SCALE = pow(2.0f, N); OUT += FUNC; }
 
+const static float3 MIN_HEIGHT = float3(1.0f, 0.15f, 1.0f);
+	
 float getDensity(float3 p)
 {
 	float d = -p.y;
@@ -10,7 +12,7 @@ float getDensity(float3 p)
 	//d += sin(n); 
 	
 	//sincos
-	//d +=sin(p.x *0.1f) * cos(p.z*0.1f) *10.0f;
+	d +=sin(p.x *0.1f) * cos(p.z*0.1f) *10.0f;
 		
 	//square block
 	//d = -0.0001;
@@ -24,14 +26,12 @@ float getDensity(float3 p)
 	//d += cos(p.z * 0.5) * sin(p.x * 0.6f) * 2.5f;
 	
 	//sandy
-	p.y *= 0.1f;
-	float n = noise3d(p *0.006f);
-	d += sin(p.x *0.01f *n) * abs(noise3d(p * 0.01f)) * 80.0f; 
-	d += sin(noise3d(p*0.1f)); // bit of texture
-	//d += cos(p.z * 0.5) * sin(p.x * 0.6f) * 2.5f;
-	
-	
-	
+	//p.y *= 0.1f;
+	//float n = noise3d(p *0.006f *MIN_HEIGHT);
+	//d += sin(p.x *0.01f *n) * abs(noise3d(p * 0.01f * MIN_HEIGHT)) * 80.0f; 
+	//d += sin(noise3d(p*0.1f)); // bit of texture
+
+	//d += abs(sin(noise3d(p*0.1f)));
 	return d;
 }
 const static float3 FOG_COLOR = float3(0.9f, 0.9f, 0.9f);
