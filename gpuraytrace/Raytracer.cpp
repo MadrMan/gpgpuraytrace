3c548574c7a03f87dad5d84c10e58b2f16beb10b
Hessel Vroege <hesselvroege@hotmail.com>
2012-06-25 11:55:59 +0200

    removed flybymode timeout for -k parameter, tweaked kinect

--- gpuraytrace/Raytracer.cpp
+++ gpuraytrace/Raytracer.cpp
@@ -114,7 +114,7 @@ void Raytracer::run(const Mode& mode, const std::string landscape)
 	timer->update(); //Update for loading time
 	
 	float lastTimeTrackingKinect = timer->getTime(); 
-	const float timeOutKinect = 30.0f;
+	const float timeOutKinect = 5.0f;
 
 	if(mode.recordMode) 
 	{
@@ -122,7 +122,7 @@ void Raytracer::run(const Mode& mode, const std::string landscape)
 		flyby->reset();
 		isFlybyMode = true;
 	}
-
+	bool kinectIsTracking = false;
 	while(escape->getState() < 0.5f)
 	{
 		//reset position
@@ -141,8 +141,23 @@ void Raytracer::run(const Mode& mode, const std::string landscape)
 		
 		timer->update();
 		float thisFrameTime = mode.fixedFrameRate ?  1.0f / TARGET_FRAME_RATE : timer->getConstant();
-		bool kinectIsTracking = camera->isTracking();
-		if(kinectIsTracking){ lastTimeTrackingKinect = timer->getTime(); }
+		
+		if(mode.autoFlyby)
+		{	
+			bool isTracking = camera->isTracking();
+			if(isTracking != kinectIsTracking)
+			{
+				kinectIsTracking = isTracking;
+				
+				if(kinectIsTracking) 
+				{
+					Logger() << "Kinect started tracking. Stopping flyby.";
+				} else {
+					Logger() << "Kinect stopped tracking. Starting flyby.";
+				}
+				isFlybyMode = !isTracking;
+			}
+		}
 
 		if(!mode.fixedFrameRate)
 		{
@@ -164,17 +179,6 @@ void Raytracer::run(const Mode& mode, const std::string landscape)
 			}else{
 				isFlybyMode = !isFlybyMode;
 				flyby->reset();
-			}
-		}
-
-		if(mode.autoFlyby)
-		{
-			if(kinectIsTracking){ lastTimeTrackingKinect = timer->getTime(); }
-			if(timer->getTime() - lastTimeTrackingKinect > timeOutKinect && !isFlybyMode)
-			{
-				isFlybyMode = true;
-				flyby->reset();
-				Logger() << "Kinect did not detect a person in " << timeOutKinect << " seconds. Starting flyby.";
 			}
 		}
 
